c24ef112630540a4991ea15134e99433addcef29
PontoZawrik <Psichotroniks@tut.by>
2025-12-06 11:59:23 +0300

    Добавьте файлы проекта.

---
+++ lab12/main.cpp
@@ -0,0 +1,14 @@
+#include "../../utils/utils/utils.h"
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+
+
+
+	system("pause");
+	return 0;
+}
